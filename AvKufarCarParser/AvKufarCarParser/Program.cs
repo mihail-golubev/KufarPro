d380b85420ceaffd7d98583220bf3b6605dee744
Mikhail Holubeu <mihanekmiha3105@gmail.com>
2025-04-02 00:03:18 +0300

    Add docker ignore and use of variable

--- AvKufarCarParser/AvKufarCarParser/Program.cs
+++ AvKufarCarParser/AvKufarCarParser/Program.cs
@@ -19,7 +19,7 @@ namespace AvKufarCarParser
                     services.AddLogging(configure => configure.AddSimpleConsole());
                     services.AddSingleton<HttpClient>();
                     services.AddSingleton<ITelegramBotClient>(sp =>
-                        new TelegramBotClient(Util.MikhailBotToken));
+                        new TelegramBotClient(Environment.GetEnvironmentVariable("BOT_TOKEN")));
                     services.AddSingleton<KufarProcessor>();
                     services.AddHostedService<BotService>();
                 })
