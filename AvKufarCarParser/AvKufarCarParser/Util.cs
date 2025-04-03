5a82e25d913c1c7421ff76fb29ad5e005eabe9fa
Mikhail Holubeu <mihanekmiha3105@gmail.com>
2025-04-03 12:08:34 +0300

    Use env variable

--- AvKufarCarParser/AvKufarCarParser/Util.cs
+++ AvKufarCarParser/AvKufarCarParser/Util.cs
@@ -9,7 +9,6 @@ namespace AvKufarCarParser
         public const long AlenaId = 769603864;
         public const long MaksId = 651255982;
 
-        public const string MikhailBotToken = "7938282959:AAHUtMKr1UqvsePTo48f8cNY4vUN82YOl80";
         public const string IlyaBotToken = "7401221219:AAGcFfwRghJq75JV_GByoHyIQF4H88E9S-8";
         public const string GetLink = "https://api.kufar.by/search-api/v2/search/rendered-paginated?cat=2010&cur=USD&prc=r%3A0%2C1500&rgn=2&size=10&sort=lst.d";
 
